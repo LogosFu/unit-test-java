9e7bef175d9fb7b89c5367ec855a538c3dd1b970
shiwei.fu <fusw@live.com>
2024-05-07 10:30:36 +0800

    test: update jdk to 20 and gradle to 8.7 and add some display test

--- src/test/java/test/dressmaker/DressmakerTest.java
+++ src/test/java/test/dressmaker/DressmakerTest.java
@@ -14,6 +14,29 @@ public class DressmakerTest {
 
     @Test
     @DisplayName("如果使用蓝色的线，当裁缝 Andy 做一件 T恤，则得到一个红色的XL T恤")
+    public void should_tailor_to_a_T_shirt_when_given_red_needlework_with_display() {
+        String size = "XL";
+        // Given
+        Dressmaker dressmaker = new Dressmaker("Andy");
+        // When
+        Tshirt tshirt = dressmaker.tailor(size, "RED");
+        // Then
+        assertEquals(tshirt, new Tshirt(size, "RED"));
+    }
+
+    @Test
+    @DisplayName("should tailor to a  T-shirt when given red needlework")
+    public void should_tailor_to_a_T_shirt_when_given_red_needlework_with_display_en() {
+        String size = "XL";
+        // Given
+        Dressmaker dressmaker = new Dressmaker("Andy");
+        // When
+        Tshirt tshirt = dressmaker.tailor(size, "RED");
+        // Then
+        assertEquals(tshirt, new Tshirt(size, "RED"));
+    }
+
+    @Test
     public void should_tailor_to_a_T_shirt_when_given_red_needlework() {
         String size = "XL";
         // Given
@@ -24,8 +47,19 @@ public class DressmakerTest {
         assertEquals(tshirt, new Tshirt(size, "RED"));
     }
 
-    @ParameterizedTest(name="如果使用蓝色的线，当裁缝 Andy 做一件 T恤，则得到一个蓝色的{0} T恤")
-    @ValueSource(strings = { "XL", "XXL" })
+    @Test
+    public void shouldTailorToA_TShirtWhenGivenRedNeedlework() {
+        String size = "XL";
+        // Given
+        Dressmaker dressmaker = new Dressmaker("Andy");
+        // When
+        Tshirt tshirt = dressmaker.tailor(size, "RED");
+        // Then
+        assertEquals(tshirt, new Tshirt(size, "RED"));
+    }
+
+    @ParameterizedTest(name = "如果使用蓝色的线，当裁缝 Andy 做一件 T恤，则得到一个蓝色的{0} T恤")
+    @ValueSource(strings = {"XL", "XXL"})
     public void should_tailor_to_a_T_shirt_when_given_blue_needlework(String size) {
         // Given
         Dressmaker dressmaker = new Dressmaker("Andy");
@@ -35,8 +69,8 @@ public class DressmakerTest {
         assertEquals(tshirt, new Tshirt(size, "BLUE"));
     }
 
-    @ParameterizedTest(name="如果使用蓝色的线，当裁缝 Andy 做一件 T恤，则得到一个{1}的{0} T恤")
-    @CsvSource({ "XL, BLUE", "XXL, RED" })
+    @ParameterizedTest(name = "如果使用蓝色的线，当裁缝 Andy 做一件 T恤，则得到一个{1}的{0} T恤")
+    @CsvSource({"XL, BLUE", "XXL, RED"})
     public void should_tailor_to_a_T_shirt_when_given_needlework(String size, String color) {
         // Given
         Dressmaker dressmaker = new Dressmaker("Andy");
@@ -55,7 +89,7 @@ public class DressmakerTest {
 
     @RepeatedTest(value = 10, name = "{displayName}{currentRepetition}/{totalRepetitions}次")
     @DisplayName("重复Case：")
-    public void repeat_10_times(RepetitionInfo repetitionInfo){
+    public void repeat_10_times(RepetitionInfo repetitionInfo) {
         System.out.println(repetitionInfo.getCurrentRepetition());
     }
 }
